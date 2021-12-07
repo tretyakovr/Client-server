449ce6137fa50af36e1af0050841c620373ce1c6
rtretyakov <rtretyakov@outlook.com>
2021-12-07 12:21:10 +1000

    Lesson06 completed

---
+++ Lesson06/server.py
@@ -0,0 +1,80 @@
+from socket import *
+import sys, logging, log.client_log_config, log.server_log_config
+from service import encode_msg, decode_msg, create_socket, encode_reply, parse_server_params, get_logger
+from decos import log
+
+
+@log
+def make_connection(s, h, p):
+    s.bind((h, p))
+    s.listen(5)
+    conn, address = s.accept()
+    if conn:
+        log.info(f'Установлено соединение с {conn} {address}')
+    else:
+        log.error(f'Ошибка установления соединения')
+        sys.exit(1)
+
+    return conn
+
+
+@log
+def send_msg(c, m):
+    m = encode_reply(m)
+    c.send(m)
+
+
+@log
+def exchange_msg(c):
+    while True:
+        msg = c.recv(1024)
+        msg = decode_msg(msg)
+        log.info(f'Получено сообщение от клиента: {msg["message"]}')
+        send_msg(c, 'Уведомление об успешном получении сообщения')
+
+        if msg['action'] == 'quit':
+            break
+
+
+@log
+def close_connection(c):
+    log.info('Останавливаем сервер')
+    c.close()
+    log.info('Свервер остановлен')
+
+
+@log
+def get_logger():
+    l = None
+    try:
+        l = logging.getLogger('server')
+    except Exception as e:
+        print(f'Возникла ошибка при создании логгера {str(e)}')
+        sys.exit(1)
+    else:
+        l.info(f'Логгер успешно создан: {l}')
+
+    return l
+
+"""
+1. Стартуем логгирование
+2. Проверяем параметры командной строки
+3. Создаем сокет
+4. Обмениваемся сообщениями с с сервером
+    1. Принимаем сообщение от клиента
+    2. Возвращаем клиенту статус 
+5. При получении запроса на разрыв закрываем соединение
+"""
+if __name__ == '__main__':
+    log = get_logger()
+    if log:
+        hosts, port = parse_server_params(sys.argv)
+        log.info(f'Определены параметры командной строки: host = {hosts}, port = {port}')
+
+        if hosts is None or port is None:
+            log.critical(f'Некорректные параметры командной строки: {sys.argv}')
+        else:
+            socket = create_socket('server')
+            client = make_connection(socket, hosts, port)
+            exchange_msg(client)
+            close_connection(client)
---
+++ Lesson06/service.py
@@ -0,0 +1,123 @@
+from socket import *
+import sys
+import json
+import logging
+from decos import log
+
+
+@log
+def parse_client_params(params):
+    host, port = None, None
+
+    if len(params) == 1:
+        host = 'localhost'
+        port = 7777
+
+    elif len(params) == 3:
+        if params[1] == 'addr':
+            host = params[2]
+            port = 7777
+        elif params[1] == 'port':
+            host = 'localhost'
+            port = params[2]
+
+    elif len(params) == 5:
+        if params[1] == 'addr':
+            host = params[2]
+        elif params[1] == 'port':
+            port = params[2]
+
+        if params[3] == 'addr':
+            host = params[4]
+        elif params[3] == 'port':
+            port = params[4]
+
+    return host, port
+
+
+@log
+def parse_server_params(params):
+    hosts, port = None, None
+
+    if len(params) == 1:
+        hosts = ''
+        port = 7777
+
+    elif len(params) == 3:
+        if params[1] == '-a':
+            hosts = params[2]
+            port = 7777
+        elif params[1] == '-p':
+            hosts = ''
+            port = params[2]
+
+    elif len(params) == 5:
+        if params[1] == '-a':
+            hosts = params[2]
+        elif params[1] == '-p':
+            port = params[2]
+
+        if params[3] == '-a':
+            hosts = params[4]
+        elif params[3] == '-p':
+            port = params[4]
+
+    return hosts, port
+
+
+@log
+def encode_msg(msg):
+    if msg == 'close_connection':
+        msg = {'action': 'quit',
+               'message': msg}
+    else:
+        msg = {'action': 'msg',
+               'message': msg}
+
+    return json.dumps(msg).encode('utf-8')
+
+
+@log
+def encode_reply(reply):
+    if reply == 'quit':
+        reply = {'action': 'quit',
+                 'status': 600}
+    else:
+        reply = {'action': 'reply',
+                 'status': 200,
+                 'message': reply}
+
+    return json.dumps(reply).encode('utf-8')
+
+
+@log
+def decode_msg(msg):
+    return json.loads(msg.decode('utf-8'))
+
+
+@log
+def create_socket(logger):
+    s = None
+    try:
+        s = socket(AF_INET,SOCK_STREAM)  # Создать сокет TCP
+    except Exception as e:
+        logging.getLogger(logger).error(f'Ошибка при создании сокета {str(e)}')
+        sys.exit(1)
+    else:
+        logging.getLogger(logger).info(f'Сокет успешно создан: {s}')
+
+    return s
+
+
+@log
+def get_logger(name_logger):
+    l = None
+    try:
+        l = logging.getLogger(name_logger)
+    except Exception as e:
+        print(f'Возникла ошибка при создании логгера {name_logger} {str(e)}')
+        sys.exit(1)
+    else:
+        l.info(f'Логгер успешно создан: {l}')
+
+    return l
